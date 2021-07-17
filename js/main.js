860d4304d621ba91a56c252a48ad87d4cd5f1e2c
mo3az <mo3az.abdellatif@gmail.com>
2021-07-17 21:21:19 +0200

    posy page

--- js/main.js
+++ js/main.js
@@ -62,6 +62,9 @@ $(document).ready(function () {
         $(".service-related-block .service-grid-item").wrap("<div class='swiper-slide'></div>");
         $(".service-related-block .services-grid").addClass("swiper-wrapper").removeClass("services-grid");
 
+        $(".single-aside .small-blog").wrap("<div class='swiper-slide'></div>");
+        $(".single-aside .related-blog").addClass("swiper-wrapper").removeClass("related-blog");
+
         var swiper = new Swiper('.service-projects-block .swiper-container', {
             spaceBetween: 15,
             loop: true,
@@ -89,6 +92,25 @@ $(document).ready(function () {
             // },
             pagination: {
                 el: '.service-related-block .swiper-pagination',
+                clickable: true,
+            },
+            breakpoints: {
+                0: {
+                    slidesPerView: 1,
+                },
+                767: {
+                    slidesPerView: 2,
+                },
+            },
+        });
+        var swiper = new Swiper('.single-aside .swiper-container', {
+            spaceBetween: 15,
+            loop: true,
+            // autoplay: {
+            //     delay: 5000,
+            // },
+            pagination: {
+                el: '.single-aside .swiper-pagination',
                 clickable: true,
             },
             breakpoints: {
